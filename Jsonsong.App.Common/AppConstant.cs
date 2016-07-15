6f2c71a42aad573b6b369db3554733e6c4c734a0
jsonsong <sj178220709@163.com>
2016-07-15 23:11:55 +0800

    添加项目文件。

---
+++ Jsonsong.App.Common/AppConstant.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Jsonsong.App.Common
+{
+    public class AppConstant
+    {
+        public static string GetHello()
+        {
+            return "this is from lib .net fw 4.6.1";
+        }
+    }
+}
---
+++ src/LibCore/SomeData.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LibCore
+{
+    public class SomeData
+    {
+        public static string GetHello()
+        {
+            return "this is from lib .net core";
+        }
+    }
+}
